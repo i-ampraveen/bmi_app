39f390055651d695de4f355ecbab14718cbef6c5
Praveen S <praveen26mysore@gmail.com>
2021-02-26 19:58:37 +0530

    Raised Button defined separately

--- lib/components/Button_Text_TextStyle.dart
+++ lib/components/Button_Text_TextStyle.dart
@@ -23,3 +23,46 @@ class btnText extends StatelessWidget {
     );
   }
 }
+
+class calculateButton extends StatelessWidget {
+
+  final Color btnColor;
+  final Color txtColor;
+  final String text;
+  final double width;
+  final double height;
+  final double size;
+  final Function callback;
+
+  calculateButton({@required this.btnColor, @required this.txtColor, @required this.text, @required this.width,
+                    @required this.height, @required this.size, @required this.callback});
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      padding: EdgeInsets.zero,
+      color: btnColor,
+      shape: RoundedRectangleBorder(
+          borderRadius: new BorderRadius.circular(10.0)
+      ),
+      child: Container(
+        width: width,
+        height: height,
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+                fontSize: (size ?? 16.0),
+                color: txtColor,
+                fontFamily: 'San francisco',
+                letterSpacing: 2.0,
+                wordSpacing: 2.0
+            ),
+          ),
+        ),
+      ),
+      onPressed: () => callback()
+    );
+  }
+}
+
